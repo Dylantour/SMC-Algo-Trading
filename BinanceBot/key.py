ef0644300938d9a230e77dcf195cc58e33fd6821
Dylantour <74725885+Dylantour@users.noreply.github.com>
2025-03-08 16:21:51 +0200

    hello world

--- BinanceBot/key.py
+++ BinanceBot/key.py
@@ -4,6 +4,6 @@ import dotenv
 dotenv.load_dotenv()
 
 # Use provided API keys - adding 'e' prefix to secret key that was missing
-key = "xYuufQfbXZAEjiK6hhfCuuNeVmRBRGAk6fCpyLUXWpZuenqc5olRuRwn82NzvwCY"
-secret = "eWqt0YeMRRELycEZFS9haV0n9FCbiJEOi0E9wtJHiUzPSwLhCa0lTX6yzvr9BXrH"
+key = os.getenv("BINANCE_API_KEY")
+secret = os.getenv("BINANCE_API_SECRET")
 
